dfa060b5d4fe17529c024481c6156216cfef8c1a
Hannah Beal <hb496721@ohio.edu>
2023-01-25 17:25:23 -0500

    Adding the lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ *    File: git-lab-program.cc
+ *    Author: Hannah Beal
+ *    Date: 1/25/2023
+ *    Description: Writing code for lab2.
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+ int main(int argc, char const *argv[]) {
+     cout << "Hello Git!!" << endl;
+     return 0;
+ }// main
